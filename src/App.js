5823b237ee14f8e767dd5e350b12a49c41ed3e31
muzahid604 <604muzahid@gmail.com>
2022-06-08 22:07:24 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div >
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1.1" d="M0,160L80,144C160,128,320,96,480,80C640,64,800,64,960,69.3C1120,75,1280,85,1360,90.7L1440,96L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
+    </div>
+  );
+}
+
+export default App;
